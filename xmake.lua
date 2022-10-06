df6d7989ffdb98c779f811ec839395a223740344
ChirmyRam <office@chirmyram.top>
2022-10-06 23:46:23 +0000

    backup

---
+++ xmake.lua
@@ -0,0 +1,30 @@
+includes("VC-LTL5.lua")
+
+add_rules("mode.debug", "mode.release")
+
+set_warnings("more")
+
+add_defines("WIN32", "_WIN32")
+add_defines("UNICODE", "_UNICODE", "_CRT_SECURE_NO_WARNINGS", "_CRT_NONSTDC_NO_DEPRECATE")
+
+if is_mode("release") then
+    add_defines("NDEBUG")
+    add_cxflags("/O2", "/Os", "/Gy", "/MT", "/EHsc", "/fp:precise")
+    add_ldflags("/DYNAMICBASE", "/LTCG")
+end
+
+add_cxflags("/utf-8")
+
+add_links("gdiplus", "kernel32", "user32", "gdi32", "winspool", "comdlg32")
+add_links("advapi32", "shell32", "ole32", "oleaut32", "uuid", "odbc32", "odbccp32")
+
+target("edge_plus")
+    set_kind("shared")
+    set_targetdir("$(buildir)/release")
+    set_basename("KBDUS")
+    add_files("src/*.cpp")
+    add_links("user32")
+    after_build(function (target)
+        os.rm("$(buildir)/release/KBDUS.exp")
+        os.rm("$(buildir)/release/KBDUS.lib")
+    end)
